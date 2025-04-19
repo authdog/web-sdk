96e8b1c0d1f78ec621ca186835d349ca1138f95d
David <david@barrat.io>
2025-04-19 22:05:36 +0200

    testing react-elements inside next demo

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -18,6 +18,7 @@ import {
 import { LogoutDropdown } from "@/components/auth/logout-dropdown";
 import { getServerSidePayloadPublicKey } from "@authdog/nextjs-app/dist/index.server";
 import { LogoutButton } from "@/components/auth/logout-btn";
+import { Navbar } from "@authdog/react-elements";
 
 export default async function Dashboard() {
   const publicKey = process.env.PK_AUTHDOG as string;
@@ -28,6 +29,7 @@ export default async function Dashboard() {
 
   return (
     <div className="flex min-h-screen flex-col">
+      <Navbar />
       <header className="sticky top-0 z-10 flex h-16 items-center justify-between border-b bg-background px-4 md:px-6">
         <div className="flex items-center gap-4">
           <Link href="#" className="flex items-center gap-2 font-semibold">
