5e3fbd85fa77ab58832ad4c29a2a58981cd49707
David <david@barrat.io>
2025-05-18 03:31:30 +0200

    add postcss-cli to the react-element process

---
+++ .changeset/angry-cups-listen.md
@@ -0,0 +1,6 @@
+---
+"@authdog/react-elements": patch
+"nextjs-app": patch
+---
+
+add postcss cli to generate css bundle to be used in consumer app
--- apps/nextjs-app/app/layout.tsx
+++ apps/nextjs-app/app/layout.tsx
@@ -1,5 +1,5 @@
-// import "@authdog/react-elements/global.css";
-import "./globals.css";
+import "@authdog/react-elements/styles.css";
+// import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { JSX } from "react";
