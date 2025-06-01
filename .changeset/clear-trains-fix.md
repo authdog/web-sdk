bfbbef5bd341919ae720d51e132ed71fcff1c26d
David <david@barrat.io>
2025-06-02 01:35:57 +0200

    export placeholder alert component

---
+++ .changeset/clear-trains-fix.md
@@ -0,0 +1,5 @@
+---
+"@authdog/react-elements": patch
+---
+
+add placeholder-alert to exports
--- packages/react-elements/src/index.ts
+++ packages/react-elements/src/index.ts
@@ -1,3 +1,4 @@
 export {Button} from "./components/ui/button";
 export {Navbar} from "./components/core/navbar";
 export {UserProfile} from "./components/core/user-profile";
+export {PlaceholderAlert} from "./components/core/placeholder-alert";
